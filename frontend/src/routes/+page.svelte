a82f6573903d654594254d9bda04e0e1e3a2d62d
lebrancconvas <lebrancconvas@gmail.com>
2023-06-01 11:31:52 +0700

    💄 feat(style): decorate UI

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -15,7 +15,44 @@
   <section id="form-section">
     <form action="">
       <input type="text" name="url" id="url" placeholder="Enter URL">
-      <input type="submit" value="Shorten">
+      <input type="submit" id="submit" value="Shorten">
     </form>
   </section>
 </main>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Chakra Petch', sans-serif;
+  }
+
+  h1 {
+    font-size: 45px;
+  }
+
+  #url {
+    width: 30%;
+    height: 30px;
+    font-size: 20px;
+  }
+
+  #submit {
+    border: none;
+    border-radius: 5px;
+    background-color: rgb(49, 148, 241);
+    color: white;
+    padding: 10px;
+    font-size: 20px;
+  }
+
+  #submit:hover {
+    cursor: pointer;
+    background-color: rgb(49, 148, 241, 0.8);
+  }
+
+  #submit:active {
+    transform: scale(0.95);
+  }
+</style>
